a23595f79760a76e7c82e228a0f6c12f99efb00e
Justin Torres <touchofsarcasm@gmail.com>
2018-01-14 01:16:51 -0500

    initial xcode added

---
+++ PorfolioApp/PorfolioApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PorfolioApp
+//
+//  Created by Justin Torres on 1/14/18.
+//  Copyright © 2018 TorresJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
